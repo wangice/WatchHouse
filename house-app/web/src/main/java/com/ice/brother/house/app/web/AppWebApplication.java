3c659841c083e93dedf52e84b6b5e0fc931ea451
ice <1163569270@qq.com>
2018-07-25 00:11:43 +0800

    升级SpringBoot包

--- house-app/web/src/main/java/com/ice/brother/house/app/web/AppWebApplication.java
+++ house-app/web/src/main/java/com/ice/brother/house/app/web/AppWebApplication.java
@@ -1,12 +1,16 @@
 package com.ice.brother.house.app.web;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.CommonsClientAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.HystrixAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author:ice
@@ -14,7 +18,7 @@ import org.springframework.context.annotation.PropertySource;
  */
 @ServletComponentScan
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = {WebMvcAutoConfiguration.class, CommonsClientAutoConfiguration.class, HystrixAutoConfiguration.class})
 @PropertySource(value = {"provider-${spring.profiles.active}.properties"})
 @ImportResource(value = {"classpath:provider.xml", "classpath:mqproducer-config.xml"})
 public class AppWebApplication {
@@ -23,14 +27,5 @@ public class AppWebApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(AppWebApplication.class, args);
-    synchronized (AppWebApplication.class) {
-      while (RUN.get()) {
-        try {
-          AppWebApplication.class.wait();
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-      }
-    }
   }
 }
--- house-server/src/main/java/com/ice/brother/house/server/HouseServerApplication.java
+++ house-server/src/main/java/com/ice/brother/house/server/HouseServerApplication.java
@@ -1,31 +1,25 @@
 package com.ice.brother.house.server;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.simple.SimpleMetricsExportAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.web.servlet.WebMvcMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.autoconfigure.ConfigurationPropertiesRebinderAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author:ice
  * @Date: 2018/6/15 11:36
  */
 @EnableEurekaServer
-@SpringBootApplication(exclude = {ConfigurationPropertiesRebinderAutoConfiguration.class})
+@SpringBootApplication(exclude = {ConfigurationPropertiesRebinderAutoConfiguration.class, WebMvcMetricsAutoConfiguration.class, SimpleMetricsExportAutoConfiguration.class})
 public class HouseServerApplication {
 
-  public static final AtomicBoolean RUN = new AtomicBoolean(true);
+    public static final AtomicBoolean RUN = new AtomicBoolean(true);
 
-  public static void main(String[] args) {
-    SpringApplication.run(HouseServerApplication.class, args);
-    synchronized (HouseServerApplication.class) {
-      while (RUN.get()) {
-        try {
-          HouseServerApplication.class.wait();
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-      }
+    public static void main(String[] args) {
+        SpringApplication.run(HouseServerApplication.class, args);
     }
-  }
 }
