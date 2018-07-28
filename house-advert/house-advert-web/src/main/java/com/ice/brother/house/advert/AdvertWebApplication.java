19ace83dba8fa48017bb27fbaa93a888adb52afd
wangwei <wangwei@cootf.com>
2018-07-28 14:36:50 +0800

    修改pom文件

--- house-advert/house-advert-web/src/main/java/com/ice/brother/house/advert/AdvertWebApplication.java
+++ house-advert/house-advert-web/src/main/java/com/ice/brother/house/advert/AdvertWebApplication.java
@@ -2,17 +2,20 @@ package com.ice.brother.house.advert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.cloud.client.CommonsClientAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.HystrixAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Created by 11635 on 2018/7/27 0027.
  */
-@SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
+@SpringBootApplication(exclude = {WebMvcAutoConfiguration.class, CommonsClientAutoConfiguration.class, HystrixAutoConfiguration.class})
 public class AdvertWebApplication {
 
     public static void main(String[] args) {
